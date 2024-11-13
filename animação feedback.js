3d1be590509109c5e6730caceab364a7f00d148e
Sarah <sahalmeidaf0@gmail.com>
2024-11-13 15:00:30 -0300

    Create animação feedback.js

---
+++ animação feedback.js
@@ -0,0 +1,4 @@
+// Função para redirecionar após 10 segundos
+setTimeout(function() {
+    window.location.href = "SejaBemVindo.html"; // Redireciona para a página de boas-vindas
+}, 10000); // 10.000 milissegundos = 10 segundos
